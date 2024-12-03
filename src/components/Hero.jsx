5c9a02e002757b0f428021a1a4a2d173a7797000
Jordan Chin <jc9627@g.rit.edu>
2024-12-03 10:54:07 -0500

    fixed text in hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,9 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">Jordan</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className="sm:block hidden" />
+            I am a Masters Computer Science
+            <br className="sm:block hidden" />
+            student and love to develop user <br className="sm:block hidden" />
             interfaces and web applications
           </p>
         </div>
